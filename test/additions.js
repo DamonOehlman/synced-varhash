3f20b59b19316dcfdb91ba730c888eac1198ce79
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-17 11:40:41 +1100

    Monitor additions

--- test/additions.js
+++ test/additions.js
@@ -1,6 +1,8 @@
+var cuid = require('cuid');
 var syncedHash = require('..');
 var test = require('tape');
 var createMesh = require('./helpers/create-mesh');
+var monitor = require('./helpers/monitor');
 var mesh;
 var hashes;
 
@@ -14,4 +16,11 @@ test('create the hashes', function(t) {
   hashes.forEach(function(hash) {
     t.ok(hash);
   });
+});
+
+test('add to hash:0 and look for changes in the other hashes', function(t) {
+  var id = cuid();
+
+  monitor(t, hashes, id, { foo: 'bar' });
+  hashes[0].put(id, { foo: 'bar' });
 });
