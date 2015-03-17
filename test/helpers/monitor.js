1dcb3660272ce6de7e2f5b825ee06fd961a054b1
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-17 13:51:52 +1100

    Ensure we unwrap data before diffing

--- test/helpers/monitor.js
+++ test/helpers/monitor.js
@@ -1,16 +1,17 @@
 var xdiff = require('xdiff');
+var unwrap = require('observ-unwrap');
 
 module.exports = function(t, hashes, id, expected, timeout) {
   t.plan(hashes.length);
 
   hashes.forEach(function(hash) {
     var timer = setTimeout(function() {
-      console.warn(id + ' data mismatch: ', hash.get(id), expected);
+      console.warn(id + ' data mismatch: ', unwrap(hash.get(id)), unwrap(expected));
       t.fail('item ' + id + ' has not stabilised with expected data');
     }, timeout || 1000);
 
     var stop = hash(function(data) {
-      if (! xdiff.diff(data[id], expected)) {
+      if (! xdiff.diff(unwrap(data[id]), unwrap(expected))) {
         t.pass('item ' + id + ' has stabilised with expected data');
         clearTimeout(timer);
         stop();
