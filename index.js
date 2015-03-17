af8ce307a6856c26ef1bfaeb912c2a18a9f9aa43
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-17 16:58:00 +1100

    Handle changes and deletions

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ var xdiff = require('xdiff');
   want to keep a varhash in sync between a number of entities. The case that
   I've used this with is WebRTC and it's pretty awesome :)
 
+  ## Known Limitations
+
+  While `synced-varhash` is capable of serializing nested observables (such
+  as [`observ-struct`](https://github.com/Raynos/observ-struct)) over a
+  CRDT, at this stage the deserialized data will be simple JS types on the
+  receiver ends.  For this reason, it is recommended that only simple
+  data is serialized at this time.
+
 **/
 module.exports = function(type) {
   function createSet(doc) {
@@ -29,7 +37,7 @@ module.exports = function(type) {
     }
 
     function toKey(id) {
-      return id.split(':')[1];
+      return id && id.split(':')[1];
     }
 
     // handle changes in the hash and apply the changes to the crdt correctly
@@ -45,24 +53,32 @@ module.exports = function(type) {
           doc.add(extend({ id: id, itemtype: type }, currentData));
         }
         else if (diff) {
-          console.log('got diff: ', diff);
+          existing.set(xdiff.patch(existing.state, diff));
+        }
+        else if (! data[key]) {
+          doc.rm(id);
         }
       });
     });
 
-    // handle crdt set additions
-    set.on('add', function(row) {
-      var key = toKey(row.id);
-      if (! hash.get(key)) {
-        hash.put(key, omit(row.state));
-      }
-    });
-
     // handle crdt set removals
     set.on('remove', function(row) {
+      hash.delete(toKey(row.id));
     });
 
     // handle crdt row changes
+    set.on('changes', function(row, changed) {
+      var key = toKey(changed.id);
+      var data = key && hash.get(key);
+      var diff = data && xdiff.diff(unwrap(data), omit(changed));
+
+      if (! data) {
+        hash.put(key, omit(changed));
+      }
+      else if (diff) {
+        hash.put(key, xdiff.patch(data, diff));
+      }
+    });
 
     return hash;
   }
