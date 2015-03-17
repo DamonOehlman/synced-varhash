ef9590d1e3a80c078930261a52a91144867fb47a
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-17 11:40:20 +1100

    Ensure monitoring hash values does timeout

--- test/helpers/monitor.js
+++ test/helpers/monitor.js
@@ -1,12 +1,18 @@
 var xdiff = require('xdiff');
 
-module.exports = function(t, hashes, id, expected) {
+module.exports = function(t, hashes, id, expected, timeout) {
   t.plan(hashes.length);
 
   hashes.forEach(function(hash) {
+    var timer = setTimeout(function() {
+      console.warn(id + ' data mismatch: ', hash.get(id), expected);
+      t.fail('item ' + id + ' has not stabilised with expected data');
+    }, timeout || 1000);
+
     var stop = hash(function(data) {
       if (! xdiff.diff(data[id], expected)) {
         t.pass('item ' + id + ' has stabilised with expected data');
+        clearTimeout(timer);
         stop();
       }
     });
